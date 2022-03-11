8b97103e710ef7225ef05cf45116b2f81f8dfbd2
angelo54425 <eddgasan8@gmail.com>
2022-03-11 13:13:59 -0800

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+		putchar (alph);
+	putchar('\n');
+	return (0);
+}
